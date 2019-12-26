25fcc8277510526efe6059d5d08ce8d31e0fa06e
Masaru Nemoto <16267290+nemolize@users.noreply.github.com>
2019-12-27 01:02:24 +0900

    fix: simplifies tag

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,7 @@ import * as React from "react";
 import { render } from "react-dom";
 
 function App() {
-  return (
-    <>
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </>
-  );
+  return (<>Hello World</>);
 }
 
 const rootElement = document.getElementById("root");
